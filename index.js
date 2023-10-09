03814ed012aa9dcc574831975d62b8e1375da238
UsmanSaeed787 <usmanalisaeed787@gmail.com>
2023-10-09 21:50:06 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+//ARITHMETIC OPERATORS:
+var num1 = 10;
+var num2 = 20;
+// addition
+var sum = num1 + num2;
+console.log(sum);
+//subtraction
+var minus = num1 - num2;
+console.log(minus);
+// multiplication
+var product = num1 * num2;
+console.log(product);
+// exponent (raise to power ^)
+var exponent = 2 ** 5;
+console.log(exponent);
+// division
+var division = num2 / num1;
+console.log(division);
+//division (remainder)
+var remainder = num2 / 3;
+console.log(remainder);
